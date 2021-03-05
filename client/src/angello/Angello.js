11aee95cd5204c2948a125c7ce203ea80904e556
Nishit <nishitsuthar123@gmail.com>
2021-03-05 17:05:53 +0530

    updated with adding logo and navbar

--- client/src/angello/Angello.js
+++ client/src/angello/Angello.js
@@ -13,10 +13,32 @@ var myModule = angular.module("Angello", [
   "angular-storage",
 ]);
 
-myModule.config(function(routeProvider) {
-    $routeProvider.when("/",{
-        templateUrl: "src/angello/storyboard/tmpl/storyboard.html",
-        controller: "StoryboardCtrl",
-        controllerAs: "storyboard"
-    });
+myModule.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "src/angello/storyboard/tmpl/storyboard.html",
+      controller: "StoryboardCtrl",
+      controllerAs: "storyboard",
+    })
+    .when("/dashboard", {
+      templateUrl: "src/angello/dashboard/tmpl/dashboard.html",
+      controller: "DashboardCtrl",
+      controllerAs: "dashboard",
+    })
+    .when("/users", {
+      templateUrl: "src/angello/user/tmpl/users.html",
+      controller: "UsersCtrl",
+      controllerAs: "users",
+    })
+    .when("/users/:userId", {
+      templateUrl: "src/angello/user/tmpl/user.html",
+      controller: "UserCtrl",
+      controllerAs: "myUser",
+    })
+    .when("/login", {
+      templateUrl: "src/angello/login/tmpl/login.html",
+      controller: "LoginCtrl",
+      controllerAs: "login",
+    })
+    .otherwise({ redirectTo: "/" });
 });
