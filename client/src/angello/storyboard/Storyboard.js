35ed7adc5b194725729a3ec6189e7f5a7e8e689c
Nishit <nishitsuthar123@gmail.com>
2021-03-06 12:09:15 +0530

    change story viwe board, add iteration in storyboard also

--- client/src/angello/storyboard/Storyboard.js
+++ client/src/angello/storyboard/Storyboard.js
@@ -1,1 +1,11 @@
-angular.module("Angello.common").service("StoryModel", function () {});
+angular.module("Angello.common").service("StoryModel", function () {
+    var storyboard = this;
+
+    storyboard.currentStory = null;
+    storyboard.editstory = {};
+
+    storyboard.setCurrentStory = function(story){
+        storyboard.currentStory = story;
+        storyboard.editstory = angular.copy(storyboard.currentStory);
+    }
+});
--- client/src/angello/storyboard/controllers/StoryboardController.js
+++ client/src/angello/storyboard/controllers/StoryboardController.js
@@ -1,5 +1,35 @@
-angular.module("Angello.Storyboard", ["Angello.Common"]);
-
 angular
   .module("Angello.Storyboard")
-  .controller("StoryboardCtrl", function () {});
+  .controller("StoryboardCtrl", function () {
+    var storyboard = this;
+
+    storyboard.stories =[
+      {
+        "assignee": "1",
+        "criteria": "It tests!",
+        "description": "This is a test",
+        "id": "1",
+        "reporter": "2",
+        "status": "To Do",
+        "title": "First Story",
+        "type": "Spike"
+      },
+      {
+        "assignee": "2",
+        "criteria": "It works!",
+        "description": "testing something",
+        "id": "2",
+        "reporter": "1",
+        "status": "In Progress",
+        "title": "Second Story",
+        "type": "Enhancement"
+      },
+    ];
+    storyboard.status= [
+      {name:"To Do"},
+      {name:"In Progress"},
+      {name:"Code Review"},
+      {name:"QA Review"},
+      {name:"Verified"}
+    ];
+  });
